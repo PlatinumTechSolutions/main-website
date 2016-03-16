f571c83657aef7d92eba7bdcd8a78e7fb307b569
Carl Casbolt <carl.casbolt@gmail.com>
2016-03-16 23:03:37 +0000

    style tweaks

--- sections/footer.php
+++ sections/footer.php
@@ -1,30 +1,24 @@
 
 <div id="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="bio">
-                <div class="avatar">
-                    <img src="/images/avatar.jpg" width="80" height="80" />
-                </div>
-                <p>A passionate and skilled developer, Carl uses over 10 years commercial experience in
-                various backend technologies to produce robust systems for any situation. With a passion
-                for quality and keen eye for detail, he takes pride in all projects he has contributed to.</p>
-            </div>
+    <div class="bio">
+        <div class="avatar">
+            <img src="/images/avatar.jpg" width="80" height="80" />
         </div>
-        <div class="row">
-            <div class="">
-                <ul class="social">
-                    <li><a href="tel:+447905323168"><i class="fa fa-phone"></i> call me +44 (0) 7905 323 168</a></li>
-                    <li><a href="mailto://hello@platinumtechsolutions.co.uk"><i class="fa fa-envelope"></i> hello@platinumtechsolutions.co.uk</a></li>
-                </ul>
-                <ul class="social">
-                    <li><a href="https://www.linkedin.com/in/carlcasbolt"><i class="fa fa-linkedin"></i> @CarlCasbolt on LinkedIn</a></li>
-                    <li><a href="https://github.com/carlcasbolt"><i class="fa fa-github"></i> @CarlCasbolt on GitHub</a></li>
-                    <li><a href="https://twitter.com/carlcasbolt"><i class="fa fa-twitter"></i> @CarlCasbolt on twitter</a></li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="copyright">&copy; 2016, Platinum Tech Solutions - VAT No: 225 4503 33</div>
+        <p>A passionate and skilled developer, Carl uses over 10 years commercial experience in
+        various backend technologies to produce robust systems for any situation. With a passion
+        for quality and keen eye for detail, he takes pride in all projects he has contributed to.</p>
     </div>
+    <div class="">
+        <ul class="social">
+            <li><a href="tel:+447905323168"><i class="fa fa-phone"></i> call me +44 (0) 7905 323 168</a></li>
+            <li><a href="mailto://hello@platinumtechsolutions.co.uk"><i class="fa fa-envelope"></i> hello@platinumtechsolutions.co.uk</a></li>
+        </ul>
+        <ul class="social">
+            <li><a href="https://www.linkedin.com/in/carlcasbolt"><i class="fa fa-linkedin"></i> @CarlCasbolt on LinkedIn</a></li>
+            <li><a href="https://github.com/carlcasbolt"><i class="fa fa-github"></i> @CarlCasbolt on GitHub</a></li>
+            <li><a href="https://twitter.com/carlcasbolt"><i class="fa fa-twitter"></i> @CarlCasbolt on twitter</a></li>
+        </ul>
+    </div>
+
+    <div class="copyright">&copy; 2016, Platinum Tech Solutions - VAT No: 225 4503 33</div>
 </div>
