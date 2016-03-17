e84b2661f6a548ead3feee09f4f99cbb5a66e69b
Carl Casbolt <carl.casbolt@gmail.com>
2016-03-17 08:36:07 +0000

    contact form styled again

--- sections/contact.php
+++ sections/contact.php
@@ -56,24 +56,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <?php if ($emailSent === true): ?>
 
-    <div class="form row">
-        <div class="span1"></div>
-        <div class="span10 thank-you">
-            <h3>Thank you for your message, we will be in contact with you shortly.</h3>
-        </div>
-        <div class="span1"></div>
+    <div class="thank-you bg-success text-success">
+        <h3>Thank you for your message, we will be in contact with you shortly.</h3>
     </div>
 
 <?php else: ?>
     <?php if (count($errors) > 0): ?>
 
-    <div class="form row">
-        <div class="span1"></div>
-        <div class="span10 form-errors">
-            <h3>It looks like you missed something</h3>
-            <p>Please make sure you enter a name and email address so that I can contact you.</p>
-        </div>
-        <div class="span1"></div>
+    <div class="form-errors bg-danger text-danger">
+        <h3>It looks like you missed something</h3>
+        <p>Please make sure you enter a name and email address so that I can contact you.</p>
     </div>
 
     <?php endif; ?>
@@ -81,10 +73,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <form class="contact-form form-horizontal" action="/index.php#contact" method="post">
         <h3>About You</h3>
 
-        <label for="contact-name">Name<span class="required">*</label>
+        <label for="contact-name">Name<span class="required text-danger">*</label>
         <input class="form-control" id="contact-name" type="text" name="name" value="" />
 
-        <label for="contact-email">Email address<span class="required">*</span></label>
+        <label for="contact-email">Email address<span class="required text-danger">*</span></label>
         <div class="input-group">
             <span class="input-group-addon" id="contact-email-addon">@</span>
             <input class="form-control" id="contact-email" type="email" name="email" value="" aria-describedby="contact-email-addon" />
@@ -100,7 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
 
         <h3>Content</h3>
-        <label for="contact-message">Message<span class="required">*</span></label>
+        <label for="contact-message">Message<span class="required text-danger">*</span></label>
         <textarea class="form-control" id="contact-message" name="message" rows="10"></textarea>
 
         <div class="submit pull-right">
