86d371f2c8c98500920ed7ad8a047a591760d9ba
Carl Casbolt <carl@platinumtechsolutions.co.uk>
2018-01-06 20:38:41 +0000

    added a homepage controller and made a start on the footer

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomepageController extends Controller
+{
+    /**
+     *@Route("/")
+     */
+    public function index()
+    {
+        return $this->render('homepage.html.twig');
+    }
+}
