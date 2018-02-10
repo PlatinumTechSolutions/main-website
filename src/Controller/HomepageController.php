c16c36df8ba62d9986d665d3b38b0d55a3ccb864
Carl Casbolt <carl@platinumtechsolutions.co.uk>
2018-02-10 20:42:20 +0000

    added a few past clients to the homepage

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -13,6 +13,41 @@ class HomepageController extends Controller
      */
     public function index()
     {
-        return $this->render('homepage.html.twig');
+        return $this->render('homepage.html.twig', [
+          'clients' => $this->getPastClients(),
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    private function getPastClients()
+    {
+        return [
+            [
+                'name' => 'New Scientist',
+                'logo' => 'NewScientist.png',
+            ],
+            [
+                'name' => 'Squawka',
+                'logo' => 'Squawka.png',
+            ],
+            [
+                'name' => 'Voucher Codes',
+                'logo' => 'VoucherCodes.png',
+            ],
+            [
+                'name' => 'Cognition X',
+                'logo' => 'CognitionX.png',
+            ],
+            [
+                'name' => 'Rightster',
+                'logo' => 'Rightster.png',
+            ],
+            [
+                'name' => 'IPC Meda',
+                'logo' => 'IPCMedia.png',
+            ],
+        ];
     }
 }
