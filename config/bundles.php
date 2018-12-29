98adbff6bf21472e96623381b54001cf4849c4fb
Carl Casbolt <carl@platinumtechsolutions.co.uk>
2018-12-29 22:12:16 +0000

    address & symfony version update

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
