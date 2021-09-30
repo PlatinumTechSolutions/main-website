058e5e5a93e66ff96d051c26bb87b84808d119a4
Carl Casbolt <carl@platinumtechsolutions.co.uk>
2021-09-30 16:56:31 +0100

    Upgraded to SF 5.3.x

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use Swift_Mailer;
-use Swift_Message;
 use App\Form\ContactType;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/contact")
@@ -17,12 +19,14 @@ class ContactController extends AbstractController
     /**
      * @Route("/", name="contact")
      */
-    public function contact(Request $request, Swift_Mailer $mailer)
-    {
+    public function contact(
+        Request $request,
+        MailerInterface $mailer
+    ): Response {
         $form = $this->createForm(ContactType::class, null, [
             'action' => $this->generateUrl('contact'),
             'attr' => [
-              'id' => 'contact-form',
+                'id' => 'contact-form',
             ],
         ]);
 
@@ -31,12 +35,17 @@ class ContactController extends AbstractController
         if ($success = ($form->isSubmitted() && $form->isValid())) {
             $contactFormData = $form->getData();
 
-            $message = (new Swift_Message(date('l jS \of F Y h:i:s A')))
-                ->setFrom(['no-reply@platinumtechsolutions.co.uk' => "Platinum Tech Solutions"])
-                ->setTo('contracts@platinumtechsolutions.co.uk')
-                ->setBody($this->renderView('emails/contact.html.twig', [
+            $message = (new TemplatedEmail())
+                ->to(new Address('contracts@platinumtechsolutions.co.uk'))
+                ->from(new Address(
+                    'no-reply@platinumtechsolutions.co.uk',
+                    'Platinum Tech Solutions'
+                ))
+                ->subject(date('l jS \of F Y h:i:s A'))
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
                     'form_data' => $contactFormData,
-                ]), 'text/html');
+                ]);
 
             $mailer->send($message);
         }
